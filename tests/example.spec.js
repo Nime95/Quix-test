149ed5a6ddd06b16825f25f2d3740cc6087cd211
Nime95 <zannatunnime95@gmail.com>
2022-01-19 13:57:16 +0600

    remove 1st test and add onther test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,13 +1,4 @@
 const { test, expect } = require("@playwright/test");
-//  test("should log in when the correct credential is provided", async ({page,}) => {
-
-//   await page.goto("http://localhost:8080/joomla4/Joomla4/administrator/");
-//   await page.fill("#mod-login-username", "admin");
-//   await page.fill("#mod-login-password", "zannatunnime2018");
-//   await page.click("#btn-login-submit");
-//   const text = await page.innerText(".page-title");
-//   expect(text.trim()).toBe("Home Dashboard");
-//   });
 
 
 
